dcc56fce840891eac059025bbb62d5553e73da7e
CoderSherlock <haopengzhan@gmail.com>
2017-09-14 11:49:45 -0400

    Fix for new path

--- java/client.sh
+++ java/client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash +vx
-LIB_PATH=$"/home/yaoliu/src_code/local/libthrift-1.0.0.jar:/home/yaoliu/src_code/local/slf4j-log4j12-1.5.8.jar:/home/yaoliu/src_code/local/slf4j-api-1.5.8.jar:/home/yaoliu/src_code/local/log4j-1.2.14.jar"
+LIB_PATH=$"/home/yaoliu/src_code/local/lib/usr/local/lib/libthrift-0.10.0.jar:/home/yaoliu/src_code/local/lib/usr/local/lib/slf4j-log4j12-1.7.12.jar:/home/yaoliu/src_code/local/lib/usr/local/lib/slf4j-api-1.7.12.jar:/home/yaoliu/src_code/local/lib/usr/local/lib/log4j-1.2.17.jar"
 #simple/secure ip port
 java -classpath bin/client_classes:$LIB_PATH JavaClient simple $1 $2
